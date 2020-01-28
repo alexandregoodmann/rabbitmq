90a7aa9a86a8be1ca6b32952f1fedd33842425a9
Alexandre <alexandregoodmann@gmail.com>
2020-01-28 15:01:08 -0200

    initial

---
+++ publisher-rabbitmq/src/main/java/br/com/goodmann/publisherrabbitmq/PublisherRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package br.com.goodmann.publisherrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherRabbitmqApplication.class, args);
+	}
+
+}
---
+++ publisher-rabbitmq/src/test/java/br/com/goodmann/publisherrabbitmq/PublisherRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.goodmann.publisherrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
