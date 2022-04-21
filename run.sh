704fe459f91f87d7bdefafbc86cd3a293f20dc14
Alexandre Ferreira <alexandregoodmann@gmail.com>
2022-04-21 17:07:15 -0300

    trying to run all containers

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+clear
+
 sudo docker-compose down
 
 sudo docker rm $(sudo docker ps -aq)
 
-sudo docker rmi $(sudo docker images -aq)
+#sudo docker rmi $(sudo docker images -aq)
 
 sudo docker-compose up
